f54c808f3f5777e02d47d93a952a3f040f57b865
uncha <kyutae21c@naver.com>
2018-03-10 20:05:25 +0900

    merge

---
+++ src/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+
+	</div>
+</template>
+
+<script src=""></script>
+<script>
+	export default {
+		data () {
+			return {
+
+			}
+		},
+		created () {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
