c2b90d1bf9925c7ddd5532efa4e4f5c150eaa37b
uncha <kyutae21c@naver.com>
2018-03-12 01:20:41 +0900

    merge

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<!-- <div>
 		<el-row>
 		 <el-col :span="8" v-for="(o, index) in $store.state.instagramData.data" :key="o.id" :offset="index > 0 ? 2 : 0">
 		    <el-card :body-style="{ padding: '0px' }">
@@ -14,57 +14,107 @@
 		    </el-card>
 		  </el-col>
 		</el-row>
-	</div>
+	</div> -->
+	<waterfall
+		align="center"
+    :line-gap="200"
+    :min-line-gap="100"
+    :max-line-gap="220"
+    :single-max-width="300"
+		auto-resize="true"
+	>
+	  <!-- each component is wrapped by a waterfall slot -->
+	  <waterfall-slot
+	  	v-for="(item, index) in $store.state.instagramData.data"
+	    :width="item.images.standard_resolution.width"
+	    :height="item.images.standard_resolution.height + (contentsHeightArr[index] || 0)"
+	    :order="index"
+      ref="waterfall"
+	    move-class="item-move"
+	  >
+	    <div class="slot-wrap">
+        <div class="cont-wrap">
+        	<img style="width:100%; height:100%" :src="item.images.standard_resolution.url" class="image" />
+	        <div ref="infoBox" class="cont">
+	        	<span class="text">{{ item.caption.text }}</span>
+		        <div class="bottom clearfix">
+		          <time class="time">{{ item.created_time }}</time>
+		          <el-button type="primary" class="button">Operating button</el-button>
+		        </div>
+	        </div>
+        </div>
+      </div>
+	  </waterfall-slot>
+	</waterfall>
 </template>
 
 <script type="text/javascript">
 	import $ from 'jquery'
+	import Waterfall from 'vue-waterfall/lib/waterfall'
+	import WaterfallSlot from 'vue-waterfall/lib/waterfall-slot'
 
 	export default {
+		components: {
+			Waterfall,
+			WaterfallSlot,
+		},
 		data () {
 			return {
 				keyword: '김수현',
+				contentsHeightArr: [],
 			}
+		},
+		computed: {
+
 		},
 		created () {
 			const token = this.$route.query.accessToken
-			console.log('token = ' + token)
 
 			this.$store.dispatch('instagramLoad', {token:token, keyword:this.keyword}).then(()=>{
-				console.log(this.$store.state.instagramData)
-			});
-		}
+				for(let i in this.$refs.infoBox){
+					setTimeout(()=>{
+						let origin = this.$store.state.instagramData.data[i].images.standard_resolution.width
+						let change = $(this.$refs.infoBox[i]).outerWidth(true)
+						let per = origin / change
+
+						this.contentsHeightArr.push($(this.$refs.infoBox[i]).outerHeight(true) * per)
+					}, 1000)
+				}
+			})
+		},
 	}
 </script>
 
 <style scoped>
-	 .time {
-    font-size: 13px;
-    color: #999;
-  }
-  
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
+	.item-move {
+    transition: all .5s cubic-bezier(.55,0,.1,1);
+    -webkit-transition: all .5s cubic-bezier(.55,0,.1,1);
   }
 
-  .button {
-    padding: 0;
-    float: right;
+  .slot-wrap{
+  	padding:7px;
   }
 
-  .image {
-    width: 100%;
-    display: block;
+  .cont-wrap{
+  	padding:15px;
   }
 
-  .clearfix:before,
-  .clearfix:after {
-      display: table;
-      content: "";
+  .cont {
+  	word-wrap: break-word;
   }
-  
-  .clearfix:after {
-      clear: both
+
+  .text {
+  	margin:10px 0;
+  	display: block;
+  }
+
+  .time{
+  	display: block;
+  	margin:5px 0;
+  }
+
+  .cont-wrap{
+  	background: #EFEFEF;
+  	border-radius: 5px;
   }
 </style>
